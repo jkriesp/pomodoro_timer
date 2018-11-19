031a1488d3bcc1a7a276f80b507b0bc56786a2e3
Jens Kristian Espevik <jenskristian@gmail.com>
2018-11-19 10:06:23 +0100

    add start/stop functionality

--- script/main.js
+++ script/main.js
@@ -1,10 +1,11 @@
 let timeContainer = document.querySelector('.timeContainer');
 let timeElement = document.createElement('p');
-    timeElement.setAttribute('id', 'time');
+timeElement.setAttribute('id', 'time');
+let myCountdown = null;
 
 function startTimer(duration, display) {
     let timer = duration, minutes, seconds;
-    setInterval(function () {
+    myCountdown = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -13,13 +14,17 @@ function startTimer(duration, display) {
 
         display.textContent = minutes + ":" + seconds;
 
-        if(--timer < 0) {
+        if (--timer < 0) {
             timer = duration;
         }
     }, 1000);
 }
 
-window.onload = function () {
-    let fiveMinutes = 60 * 5;
-    startTimer(fiveMinutes, timeContainer);
-};
+document.getElementById('btn_start').addEventListener('click', function () {
+    let duration = 60 * 25;
+    startTimer(duration, timeContainer);
+});
+
+document.getElementById('btn_stop').addEventListener('click', function () {
+    clearInterval(myCountdown);
+});
