07ebdfbd39d8a7ba3061eaccce5a59f5634d619c
Jens Kristian Espevik <jenskristian@gmail.com>
2018-11-19 09:03:02 +0100

    change countdown logic

--- script/main.js
+++ script/main.js
@@ -1,36 +1,25 @@
 let timeContainer = document.querySelector('.timeContainer');
 let timeElement = document.createElement('p');
-timeElement.setAttribute('id', 'time');
+    timeElement.setAttribute('id', 'time');
 
-let now = new Date().getMinutes();
-let countDownLength = 25;
-let countDownTo = "";
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-if (now + countDownLength >= 60) {
-    countDownTo = now + countDownLength - 60;
-} else {
-    countDownTo = now + countDownLength;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if(--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 }
 
-//timeElement.innerText = countDownTo;
-//timeContainer.appendChild(timeElement);
-
-let x = setInterval(function () {
-    // Get minutes
-    let now = new Date().getMinutes();
-    let nowSeconds = new Date().getSeconds();
-
-    // Distance between now and countDownTo
-    let minutes = 0;
-    if (countDownTo - now <= 0) {
-        minutes = countDownTo - now + 60;
-    } else {
-        minutes = countDownTo - now;
-    }
-    
-    let seconds = 60 - nowSeconds;
-   
-
-    timeElement.innerText = minutes + "m " + seconds + "s";
-    timeContainer.appendChild(timeElement);
-}, 1000)
+window.onload = function () {
+    let fiveMinutes = 60 * 5;
+    startTimer(fiveMinutes, timeContainer);
+};
